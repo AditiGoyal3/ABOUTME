7b884efb45c0053aec4d734b4ce73cad5fed7c17
tara <goldensuperhero967@gmail.com>
2023-07-13 13:04:47 -0700

    Initial Commit

---
+++ ABOUTME/ABOUTMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABOUTMEApp.swift
+//  ABOUTME
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ABOUTMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
